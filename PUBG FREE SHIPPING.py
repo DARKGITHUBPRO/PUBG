2d9950f56a84b2a6e4efe68bb0498ed97ce5f373
DARKGITHUBPRO <111252375+DARKGITHUBPRO@users.noreply.github.com>
2023-09-07 10:40:08 +0300

    Update PUBG FREE SHIPPING.py

--- PUBG FREE SHIPPING.py
+++ PUBG FREE SHIPPING.py
@@ -1,5 +1,12 @@
 print ("HI USER")
 p = int(input('Put an ID account in Pubg : '))
-w = int(input('Put The Amount You Want Charge : '))
+ d="2468"
+if p == d:
+  
+  print ('OK You Can Use The PIN CON')
+  w = int(input('Put The Amount You Want Charge : '))
+  print ('Congratulations , Please Verify Your account')
 
-print ('Congratulations , Please Verify Your account')
+else:
+  print ('OPPS , Sorry You canint Use APP ')
+
