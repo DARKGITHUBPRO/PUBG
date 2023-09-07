5ac0897b1f85b8179ec56ca3bdb9d3d610b79988
DARKGITHUBPRO <111252375+DARKGITHUBPRO@users.noreply.github.com>
2023-09-07 11:22:16 +0300

    Update PUBG CODE.py
    1- الاختيار للرقم الصحيح يعني الفوز بالرويال باس
    2- عدم الاختيار للرقم الصحيح لن تتمكن من اعاده الاختيار
    3- تحياتي مبرمج الاداه {محمد علاء}

--- PUBG FREE SHIPPING.py
+++ PUBG FREE SHIPPING.py
@@ -1,12 +1,45 @@
-print ("HI USER")
-p = int(input('Put an ID account in Pubg : '))
- d="2468"
-if p == d:
-  
-  print ('OK You Can Use The PIN CON')
-  w = int(input('Put The Amount You Want Charge : '))
-  print ('Congratulations , Please Verify Your account')
+print ("""
+███████████████████████████
+███████▀▀▀░░░░░░░▀▀▀███████
+████▀░░░░░░░░░░░░░░░░░▀████
+███│░░░░░░░░░░░░░░░░░░░│███
+██▌│░░░░░░░░░░░░░░░░░░░│▐██
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+██▌░│██████▌░░░▐██████│░▐██
+███░│▐███▀▀░░▄░░▀▀███▌│░███
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+████▄─┘██▌░░░░░░░▐██└─▄████
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+███████▄░░░░░░░░░░░▄███████
+██████████▄▄▄▄▄▄▄██████████
+███████████████████████████
 
+
+
+
+
+
+
+
+
+""")
+
+
+
+
+
+
+print ("   HI USER")
+p = int(input('   Put an ID account in Pubg : '))
+d = 2578
+if d == p:
+  print ('   OK You Can Use The PIN CON')
+  w = int(input('   Put The Amount You Want Charge : '))
+  print ('   Congratulations , Please Verify Your account')
 else:
-  print ('OPPS , Sorry You canint Use APP ')
-
+  print ('   OPPS , Sorry You canint Use APP ')
